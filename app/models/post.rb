caa9ac4335a0a9b043ba768864b63a8bfbbf424f
Krae <kraewind@gmail.com>
2021-03-15 17:21:58 -0400

    commit tree fcc47f8bf4d42108913bb076500d0b0afa7b4f70

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
+  def category_attributes=(category_attributes)
 
+    category_attributes.values.each do |category_attribute|
+  
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def users_who_commented
+    list = []
+    self.comments.each do |c|
+      list << User.find(c.user_id).username
+    end
+    thing = list.uniq
+  end
 end
